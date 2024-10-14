78cbf58040b652f4a1d063130d1f52fcb7d489ed
DianaSoares <Dianasoaresc21@gmail.com>
2024-10-14 21:42:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+   
+    const formData = new FormData(this);
+    const nome = formData.get('nome');
+    const email = formData.get('email');
+    const telefone = formData.get('telefone');
+    const estado = formData.get('estado');
+
+    
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!emailRegex.test(email)) {
+        alert('Por favor, introduza um endereço de e-mail válido.');
+        return;
+    }
+
+ 
+    const telefoneRegex = /^\d{9}$/;
+    if (!telefoneRegex.test(telefone)) {
+        alert('Por favor, introduza um número de telefone válido com 9 dígitos.');
+        return;
+    }
+
+   
+    const data = {
+        Nome: nome,
+        Email: email,
+        Telefone: telefone,
+        Zona_imóvel: estado
+    };
+
+    
+    fetch('https://formspree.io/f/xwpkkebp', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert('Dados enviados com sucesso!');
+    })
+    .catch(error => {
+        console.error('Erro:', error);
+    });
+});
